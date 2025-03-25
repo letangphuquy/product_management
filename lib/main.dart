8349c50ad250da875854d7b0150ba60f794678be
Le Tang Phu Quy <letangphuquy4@gmail.com>
2025-03-25 09:47:31 +0700

    Secret: add .env and new API keys

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ import 'package:firebase_app_check/firebase_app_check.dart';
 import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load(fileName: ".env"); // Load environment variables
+  } catch (e) {
+    throw Exception('Error loading .env file: $e'); // Print error if any
+  }
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
